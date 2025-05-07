4520783127986411fc6414957b6f597e5a73e304
Divyansh1217 <divyansh27aggarwal@gmail.com.com>
2025-05-07 10:01:43 +0530

    cleaning

--- main.py
+++ main.py
@@ -6,14 +6,37 @@ import seaborn as sns
 # calling of dataset
 df=pd.read_csv("data/data.csv")
 pd.set_option('display.max_columns', None)
-print(df.describe())
-print(df.info())
+
 
 # Cleaning
+
 df=df.drop("timestamp", axis=1)
 objects=['equipment_energy_consumption','lighting_energy','zone1_temperature','zone1_humidity','zone2_temperature']
 df[objects]=df[objects].apply(pd.to_numeric, errors='coerce')
-corr=df.corr()
-sns.heatmap(corr, annot=True, cmap='coolwarm', fmt='.2f')
-plt.title('Correlation Heatmap')
-plt.show()
+# corr=df.corr()
+# sns.heatmap(corr, annot=True, cmap='coolwarm', fmt='.2f')
+# plt.title('Correlation Heatmap')
+# plt.show()
+
+# sns.histplot(df['outdoor_temperature'], bins=30, kde=True)
+# plt.title('Distribution of Equipment Energy Consumption')   
+# plt.show()
+
+
+
+obj=['equipment_energy_consumption', 'lighting_energy', 'zone1_temperature',
+       'zone1_humidity', 'zone2_temperature', 'zone2_humidity',
+       'zone3_temperature', 'zone3_humidity', 'zone4_temperature',
+       'zone4_humidity', 'zone5_temperature', 'zone5_humidity',
+       'zone6_temperature', 'zone6_humidity', 'zone7_temperature',
+       'zone7_humidity', 'zone8_temperature', 'zone8_humidity',
+       'zone9_temperature', 'zone9_humidity', 'outdoor_temperature',
+       'atmospheric_pressure', 'outdoor_humidity', 'wind_speed',
+       'visibility_index', 'dew_point', 'random_variable1',
+       'random_variable2']
+print(df.columns)
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(strategy='mean')
+df[obj] = imputer.fit_transform(df[obj])
+print(df.isnull().sum())
+
